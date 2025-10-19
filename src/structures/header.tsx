833bd1db51d7cfa9d4813546544547ca4b494f44
Alex Saines <46796104+adsaines@users.noreply.github.com>
2025-10-19 00:16:09 -0400

    got some basic looks rolling, not pretty yet

--- src/structures/header.tsx
+++ src/structures/header.tsx
@@ -2,16 +2,43 @@
 
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import linkedIn from "@/images/linked-in-white-background.png"
 
 export const Header = ({children}: {children: React.ReactNode}) => {
     const path = usePathname()
 
     return (
         <div className="h-screen w-full flex flex-col">
-            <div data-show={path !== '/'} className={`h-8 b-2 w-full data-[show="false"]:hidden`}>
-                <Link href='resume'>Resume</Link>
-            </div>
+            <nav 
+                aria-label="Pages and contact links."
+                data-show={path !== '/'} 
+                className={`p-2 bg-cyan-600 h-10 b-2 flex w-full data-[show="false"]:hidden justify-between items-center`}
+                >
+                <div className="flex gap-1">
+                    <HeaderLink hoverText="Resume" name="Resume" path="resume" />
+                </div>
+                <div className="flex">
+                    <a 
+                        href="https://www.linkedin.com/in/AlexSaines"
+                        target="_blank"
+                        >
+                        <img className="w-8" src={linkedIn.src} alt={'Linked In'} />
+                    </a>
+                </div>
+            </nav>
             {children}
         </div>
+    )
+}
+
+const HeaderLink = ({path, name, hoverText}: {path: string, name: string, hoverText: string}) => {
+    return (
+        <Link 
+            className="bg-slate-100 rounded-lg py-1 px-2 border hover:border-2 font-medium font-black hover:font-bold hover:bg-slate-200 border-slate-700"
+            href={path} 
+            aria-label={hoverText}
+            >
+            {name}
+        </Link>
     )
 }
