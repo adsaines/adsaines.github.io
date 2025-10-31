18f8fcae362567518bfb1018609722662fff6e88
Alex Saines <46796104+adsaines@users.noreply.github.com>
2025-10-31 00:50:36 -0400

    created text files and api to read them for the story page

---
+++ src/app/api/stories/route.ts
@@ -0,0 +1,72 @@
+import { NextRequest } from "next/server"
+import * as fs from "node:fs";
+import path from "node:path";
+
+export type StoryDirections = {
+    name: string;
+    file: string;
+}
+
+export type StoryDetails = {
+    title: string;
+    story: string;
+}
+
+export async function GET(request: NextRequest) {
+
+    // TODO: migrate the rest of the stories into this object & give them each a file
+    const storyDetails: string[] = [
+        'data-buried-deep.txt',
+        'lodash-hell.txt',
+        'data-back-door.txt',
+        'legacy-code-base.txt',
+        'marble-hell.txt',
+    ]
+
+    const myStories = await Promise.all(storyDetails.map((story) => {
+        return new Promise((resolve, reject) => {
+            const fetchPath = path.resolve(`src/app/api/stories/${story}`);
+            const storyName = story.replaceAll('.txt','').replaceAll('-','_');
+
+            const processTextFile = (data: string) => {
+                return data.split('\n')
+                    .filter(line => line !== '' && line !== '\r')
+                    .map(line => line.replaceAll('\r',''))
+                    .join('<SPLIT>')
+            }
+
+            const readFileContents = (err: Error | undefined, data: string) => {
+                if(err){
+                    console.error(`Story not found: "${story}" at "${fetchPath}"`)
+                    console.error(err);
+
+                    reject({
+                        story: 'Not found.',
+                        title: storyName
+                    } as StoryDetails)
+                }
+                
+                // TODO-RE: testing only
+                console.log('Successfully processed!');
+
+                resolve({
+                    story: processTextFile(data),
+                    title: storyName
+                } as StoryDetails)
+            }
+
+            // https://nodejs.org/dist/latest-v6.x/docs/api/fs.html#fs_fs_readfile_file_options_callback
+            // NOTE: this isn't an error and works fine
+            fs.readFile(fetchPath, 'utf-8', readFileContents)
+        })
+    }))
+
+
+    return new Response(
+        JSON.stringify(myStories),
+        {
+            status: 200,
+            headers: {'Content-Type': 'application/json'}
+        }
+    )
+}
