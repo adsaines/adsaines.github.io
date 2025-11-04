3381ffbff1b47a120558aa7843c92a0dd1993e34
Alex Saines <46796104+adsaines@users.noreply.github.com>
2025-11-03 23:24:22 -0500

    reafactored link on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,25 +12,22 @@ export default function Home() {
             <h2 className="text-xl pb-16">
                 Seriously, I'm good at this.
             </h2>
-
-            <Link 
-                className="bg-(--background-theme-secondary) py-2 px-4 min-w-1/5 text-center font-semibold text-xl border-1 border-(--background-theme-secondary) hover:border-(--text-theme-primary)" 
-                href={'/resume'}
-                >
-                    Resume
-            </Link>
-            <Link 
-                className="bg-(--background-theme-secondary) py-2 px-4 min-w-1/5 text-center font-semibold text-xl border-1 border-(--background-theme-secondary) hover:border-(--text-theme-primary)" 
-                href={'/avowal'}
-                >
-                    Avowals
-            </Link>
-            <Link 
-                className="bg-(--background-theme-secondary) py-2 px-4 min-w-1/5 text-center font-semibold text-xl border-1 border-(--background-theme-secondary) hover:border-(--text-theme-primary)" 
-                href={'/stories'}
-                >
-                    Story time
-            </Link>
+            
+            <HomePageLink path="resume" title="Resume" />
+            <HomePageLink path="avowal" title="Avowals" />
+            <HomePageLink path="stories" title="Story time" />
+            <HomePageLink path="settings" title="Settings" />
         </div>
     );
+}
+
+const HomePageLink = ({title, path}:{title: string, path: string}) => {
+    return (
+        <Link 
+            className="bg-(--background-theme-secondary) py-2 px-4 min-w-1/5 text-center font-semibold text-xl border-1 border-(--background-theme-secondary) hover:border-(--text-theme-primary)" 
+            href={`/${path}`}
+            >
+                {title}
+        </Link>   
+    )
 }
