7b3081b23494ee25e5977ea84e2d81aa8558abb8
Alex Saines <46796104+adsaines@users.noreply.github.com>
2025-11-19 00:21:31 -0500

    added mobile variant to story time for better reading

--- src/app/stories/projections.tsx
+++ src/app/stories/projections.tsx
@@ -1,3 +1,5 @@
+import { Story } from "./page"
+
 export const StorySelection = ({onClick, title, selected}: {onClick: ()=>void, title: string, selected: boolean}) => {
     return (
         <button data-selected={selected} onClick={onClick} className="flex group justify-between data-[selected='true']:text-(--text-theme-secondary)">
@@ -6,5 +8,21 @@ export const StorySelection = ({onClick, title, selected}: {onClick: ()=>void, t
                 <span className="material-icons">keyboard_double_arrow_right</span>
             </div>
         </button>
+    )
+}
+
+export const StoryDisplay = ({story}:{story:Story}) => {
+    return (
+        <div className="overflow-auto flex flex-col w-full p-6 gap-4">
+            <h1 className="font-semibold text-2xl">{story.title}</h1>
+            <h2 className="font-semibold text-xl text-(--text-theme-secondary)">{story.lvl}</h2>
+            <p>
+                {story.abstract}
+            </p>
+            <hr className="w-full" />
+            {
+                story.contents.map((section, index) => ( <p key={`story-section-${index}`}>{section}</p> ))
+            }
+        </div>
     )
 }
