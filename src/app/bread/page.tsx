c5dc5f2c00309d51a6f12c71f64157759c5a0186
Alex Saines <46796104+adsaines@users.noreply.github.com>
2026-04-10 23:55:43 -0400

    Completed baking section

--- src/app/bread/page.tsx
+++ src/app/bread/page.tsx
@@ -1,5 +1,5 @@
 import { GridFactory } from "@/projections/gridTableGenerators";
-import { AmountsAndRatios, KneadingAndWaiting, WaitTimes } from "./sections";
+import { AmountsAndRatios, KneadingAndWaiting, LetsBake, WaitTimes } from "./sections";
 
 export default function Bread() {
     return (
@@ -10,9 +10,7 @@ export default function Bread() {
             <hr />
             <WaitTimes />
             <hr />
-            <section>
-                Now bake that loaf. Probably throw in a sex joke about jiggly things. Mention dutch oven, temperature, time, and loaf size. Make sure to note that crisp crust is not dependent on coating a loaf with anything but the amount of time cooked without a lid.
-            </section>
+            <LetsBake />
         </article>
     )
 }
--- src/app/bread/sections.tsx
+++ src/app/bread/sections.tsx
@@ -4,6 +4,15 @@ import knead2 from '@/images/Bread_Knead_2.jpg'
 import knead3 from '@/images/Bread_Knead_3.jpg'
 import knead4 from '@/images/Bread_Knead_4.jpg'
 import rise1 from '@/images/Bread_Rise_1.jpg'
+import rise2 from '@/images/Bread_Rise_2.jpg'
+import rise4 from '@/images/Bread_Rise_4.jpg'
+import Marking2 from '@/images/Bread_Marking_2.jpg'
+import Baking1 from '@/images/Bread_Baking_1.jpg'
+import Baking2 from '@/images/Bread_Baking_2.jpg'
+import Baking3 from '@/images/Bread_Baking_3.jpg'
+import Baked1 from '@/images/Bread_Baked_1.jpg'
+import Baked2 from '@/images/Bread_Baked_2.jpg'
+import Baked4 from '@/images/Bread_Baked_4.jpg'
 
 export const AmountsAndRatios = () => {
     return (
@@ -164,6 +173,131 @@ export const WaitTimes = () => {
             <p>
                 I like to mix, fold, and proof over the course of one day, then refrigerate over night. It adds some flavor to the bread and makes it very easy to mark (score).
             </p>
+        </section>
+    )
+}
+
+export const LetsBake = () => {
+    return (
+        <section className="flex flex-col w-full gap-4 px-8">
+            <h2 className="text-center text-xl text-(--light-secondary)">
+                Baking Time
+            </h2>
+            <div className="flex gap-4">
+                <div className="flex flex-col gap-4 justify-center">
+                    <p>
+                        It's risen! What was lumpy is now smooth and unblemished.
+                    </p>
+                    <p>
+                        Note that the dough has expanded and fills the basket fully, but is not overlowing.
+                    </p>
+                    <p>
+                        If you poke it gently, the indentation will bounce back, slowly.
+                    </p>
+                    <p>
+                        It should not be sticky or dry.
+                    </p>
+                </div>
+                <div className="flex flex-col text-center gap-2 w-2/5 mx-auto">
+                    <img src={rise2.src} alt="properly proofed"/>
+                    <p>poperly proofed loaf in it's banneton</p>
+                </div>
+            </div>
+            <div className="flex gap-4">
+                <div className="flex flex-col text-center gap-2 w-2/5 mx-auto shrink-0">
+                    <img src={Baking1.src} alt="hot pan in a hot oven"/>
+                    <p>hot oven, hot pan</p>
+                </div>
+                <div className="flex flex-col gap-4 justify-center">
+                    <p>
+                        Start your oven. Set it to 500 deg F / 260 deg C and place a <a className="text-(--external-link) hover:underline" href="https://en.wikipedia.org/wiki/Dutch_oven" target="_blank">Dutch Oven</a> or <a className="text-(--external-link) hover:underline" href="https://www.amazon.com/Lodge-Cooker-Pre-seasoned-Skillet-Convertible/dp/B0009JKG9M" target="_blank">Combo Cooker</a> the center. It also helps to have a baking stone (pizza stone) on the bottom of the oven.
+                    </p>
+                    <p>
+                        Let your oven heat up for at least 30 minutes so that the pot is piping hot.
+                    </p>
+                    <p>
+                        Once the oven and pan are both hot, we need to remove the dough and score it.
+                    </p>
+                </div>
+            </div>
+            <div className="flex gap-4">
+                <div className="flex flex-col gap-4 justify-center">
+                    <p>
+                        Once the oven and pan are both hot, we need to remove the dough and score it.
+                    </p>
+                    <p>
+                        If you have a combo cooker you can flip the dough right into the cooker. If you have a dutch oven then you'll want to use some parchment paper so that you can move the dough into the deeper well.
+                    </p>
+                    <p>
+                        Scoring / marking your loaf is required to allow the loaf to expand as it desires. The design that is made will effect how well the loaf expands. But you don't need to worry too much. A long slash from one side to the other will let it expand just fine.
+                    </p>
+                </div>
+                <div className="flex flex-col text-center gap-2 w-2/5 mx-auto shrink-0">
+                    <img src={rise4.src} alt="properly proofed"/>
+                    <p>proper proofing should retain the basket shape</p>
+                </div>
+            </div>
+            <p>
+                When you score your loaf, don't be gentle. Make sure to cut a good line. Use a very sharp knife or a razor. I have a <a className="text-(--external-link) hover:underline" href="https://www.amazon.com/Lccowot-Bread-Lame-Stainless-Sourdough/dp/B0C369NX7C/ref=sr_1_25?sr=8-25" target="_blank">razor</a>. I like to do one off-center line from end to end on all of my loaves. After that I'm just trying to draw pretty designs.
+            </p>
+            <div className="flex flex-col text-center gap-2 mx-auto shrink-0">
+                <img src={Marking2.src} alt="properly proofed"/>
+                <p>design is mostly your choice</p>
+            </div>
+            <p>
+                As an aside: note that the inside of the loaf is not solid. There should be lots of little bubbles and what looks like a set of webbed connections. This is a mark of proper yeast gestation. The internal strength of your bread is determined by this web. If it's good and strong you get a soft bouncy interior.
+            </p>
+            <p>
+                Place your loaf into you cooker. <span className="text-(--light-tertiary)">MAKE SURE TO PLACE THE LID ONTO YOUR POT</span>. And set a timer for 20 minutes.
+            </p>
+            <p>
+                It is crucial that you put the lid on the pot for the first half of the baking time because the rise of your loaf requires a moist environment. Professional ovens accomplish this by adding steam into the oven directly. Using a lidded pot traps all of the moisture in your loaf and steams it without the need to add any to your oven.
+            </p>
+            <div className="flex gap-4">
+                <div className="flex flex-col text-center gap-2 w-2/5 mx-auto shrink-0">
+                    <img src={Baking3.src} alt="halfway through"/>
+                    <p>halfway through</p>
+                </div>
+                <div className="flex flex-col gap-4 justify-center">
+                    <p>
+                        After 20 minutes, open the oven, remove the pot lid, and set the temperature to 450 deg F / 230 deg C.
+                    </p>
+                    <p>
+                        Close up the oven and set the timer for another 20 minutes.
+                    </p>
+                </div>
+            </div>
+            <div className="flex gap-4">
+                <div className="flex flex-col text-center gap-2 w-2/5 mx-auto shrink-0">
+                    <img src={Baking2.src} alt="steamed bread expands well"/>
+                    <p>steamed bread expands well</p>
+                </div>
+                <div className="flex flex-col gap-4 justify-center w-3/5">
+                    <p>
+                        Steaming a loaf helps it expand and sets you up for a good springy interior.
+                    </p>
+                </div>
+            </div>
+            <p>
+                It's done! A crunchy-crisp outside hiding a soft and stretchy inside. The expanded crisp edge to the primary lengthwise cut is called an "ear." It should be dark, but not burnt. The height of the ear indicates the amount of expansion the loaf has experienced.
+            </p>
+            <div className="flex gap-4 w-4/5 mx-auto">
+                <div className="flex flex-col text-center gap-2 w-2/5 mx-auto shrink-0">
+                    <img src={Baked1.src} alt="dark carmel crust is done"/>
+                    <p>dark carmel crust is done</p>
+                </div>
+                <div className="flex flex-col text-center gap-2 w-2/5 mx-auto shrink-0">
+                    <img src={Baked2.src} alt="the web is now stretched"/>
+                    <p>the web is now stretched</p>
+                </div>
+            </div>
+            <p>
+                Try not to cut into your bread for at least one hour. When it's fresh out of the oven the interior needs time to set. If you cut it immedietely the interior will dry out and become crumbly. Your knife will also catch on the moist interior and make for an uneven cut.
+            </p>
+            <div className="flex flex-col text-center gap-2 mx-auto shrink-0">
+                <img src={Baked4.src} alt="properly proofed"/>
+                <p>I like to make two at a time</p>
+            </div>
         </section>
     )
 }
