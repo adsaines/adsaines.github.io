4c36671cd8afbe73e514aae9bc663344a88e7517
Alex Saines <46796104+adsaines@users.noreply.github.com>
2026-04-23 23:15:43 -0400

    the carousel now sets the transition times correctly and iterates automatically through the given pictures

--- src/structures/carousel.tsx
+++ src/structures/carousel.tsx
@@ -7,9 +7,8 @@ export type CarouselPicture = {
 
 type PictureDirection = 'left' | 'right'
 
-const CAROUSEL_AUTO_GO_TIME = 2000;
-// set this to, like, a minute, once things are working
-const HUMAN_INTERACTION_WAIT_TIME = 6000;
+const CAROUSEL_AUTO_GO_TIME = 6000;
+const HUMAN_INTERACTION_WAIT_TIME = 60000;
 
 /*
     Lots of things to work out...
@@ -17,83 +16,54 @@ const HUMAN_INTERACTION_WAIT_TIME = 6000;
     1) the previous picture is still visible, it needs to go away completely
     2) sliding in from the left doesn't work right
     3) opacity isn't applying to animations
-    4) standard loop does not actually iterate the picture beyond the first one
 */
 
 export const PictureCarousel = ({ pictures }:{ pictures: CarouselPicture[] }) => {
-
-    const [useStandardTime, setUseStandardTime] = useState(false);
     const [previousIdx, setPreviousIdx] = useState(0)
     const [picIdx, setPicIdx] = useState(0);
     const [direction, setDirection] = useState<PictureDirection>('right');
+    const [trigger, setTrigger] = useState(0);
     const timeOutforNextPic = useRef<NodeJS.Timeout>(null)
 
     useEffect(() => {
-        // TODO-RE: testing only
-        console.log('carousel use effect, timeOutforNextPic', timeOutforNextPic);
-
-        if(timeOutforNextPic.current === null){
-            goToNextPicture();
-        }
-    }, [])
-
-    const goToNextPicture = () => {
-        // TODO-RE: testing only
-        console.log('goToNextPicture, picIdx: ', picIdx);
-        
-        setPreviousIdx(picIdx);
-        setDirection('right');
-        if(picIdx >= pictures.length){
-            // TODO-RE: testing only
-            console.log('goToNextPicture, picIdx >= pictures.length: ', picIdx >= pictures.length);
-
-            setPicIdx(0);
+        if(trigger !== 0){
+            goToPicture(1);
         } else {
-            // TODO-RE: testing only
-            console.log('goToNextPicture, picIdx + 1: ', picIdx + 1);
-
-            setPicIdx(picIdx + 1);
+            setupNextPicMove(CAROUSEL_AUTO_GO_TIME);
         }
-        
-        defineNextPicMove();
-    }
+    }, [trigger])
 
-    const goToPreviousPicture = () => {
-        // TODO-RE: testing only
-        console.log('goToPreviousPicture, picIdx: ', picIdx);
-        
+    const goToPicture = (increment: number,useTime = CAROUSEL_AUTO_GO_TIME) => {
+        setDirection(increment < 0 ? 'left' : 'right');
         setPreviousIdx(picIdx);
-        setDirection('left');
-        if(picIdx <= 0){
+
+        const newPicIndex = picIdx + increment;
+
+        if (newPicIndex >= pictures.length){
+            setPicIdx(0);
+        } else if (newPicIndex < 0){
             setPicIdx(pictures.length -1);
         } else {
-            setPicIdx(picIdx - 1);
+            setPicIdx(newPicIndex);
         }
-        
-        defineNextPicMove();
+
+        setupNextPicMove(useTime);
     }
 
-    const defineNextPicMove = () => {
+    const setupNextPicMove = (useTime: number) => {
+        // TODO-RE: testing only
+        console.log(useTime);
+
         if(timeOutforNextPic.current !== null){
             clearTimeout(timeOutforNextPic.current);
         }
         
-        if(useStandardTime){
-            timeOutforNextPic.current = setTimeout(
-                () => {
-                    goToNextPicture()
-                }, 
-                CAROUSEL_AUTO_GO_TIME
-            )
-        } else {
-            timeOutforNextPic.current = setTimeout(
-                () => {
-                    goToNextPicture();
-                    setUseStandardTime(true);
-                }, 
-                HUMAN_INTERACTION_WAIT_TIME
-            )
-        }
+        timeOutforNextPic.current = setTimeout(
+            () => {
+                setTrigger(trigger +1);
+            }, 
+            useTime
+        )
     }
 
     return (
@@ -114,14 +84,24 @@ export const PictureCarousel = ({ pictures }:{ pictures: CarouselPicture[] }) =>
                 }
             </div>
             <div className="flex px-8 py-2">
-                <button className="flex shrink mr-auto border border-(--light-primary)" onClick={goToPreviousPicture}>
+                <button 
+                    className="flex shrink mr-auto border border-(--light-primary)" 
+                    onClick={() => {
+                        goToPicture(-1, HUMAN_INTERACTION_WAIT_TIME);
+                    }}
+                    >
                     <span className="material-icons ">keyboard_double_arrow_left</span>
                     <span className="material-icons ">keyboard_double_arrow_left</span>
                 </button>
                 <div className="grow text-center">
                     {pictures[picIdx].subText}({picIdx +1} / {pictures.length})
                 </div>
-                <button className="flex shrink ml-auto border border-(--light-primary)" onClick={goToNextPicture}>
+                <button 
+                    className="flex shrink ml-auto border border-(--light-primary)" 
+                    onClick={() => {
+                        goToPicture(1, HUMAN_INTERACTION_WAIT_TIME);
+                    }}
+                    >
                     <span className="material-icons ">keyboard_double_arrow_right</span>
                     <span className="material-icons ">keyboard_double_arrow_right</span>
                 </button>
@@ -145,7 +125,7 @@ const PictureBox = ({
         <div 
             data-show={incoming || previousPic}
             data-direction={direction}
-            data-incoming={true}
+            data-incoming={incoming}
             className={`
                 w-full 
 
