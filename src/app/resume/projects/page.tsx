fc977582f0d3bc8af764f82393539fe05d7bc9b8
Alex Saines <46796104+adsaines@users.noreply.github.com>
2026-04-28 00:22:02 -0400

    Made a few adjustments to the basic setup for a project display and added all four accenture projects.

--- src/app/resume/projects/page.tsx
+++ src/app/resume/projects/page.tsx
@@ -1,23 +1,47 @@
 import { NextPage } from "next";
 import { Employer, Project } from "./projections";
 
+/*
+    TODO: objectify all of the project information so that it can be easily re-used for resume generation.
+*/
+
 const Projects: NextPage = () => {
     return (
         <div className="h-full overflow-auto flex justify-center mb-4">
             <article className={`resume flex flex-col md:p-4 gap-6 m-2 md:max-w-4/5 sm:w-full text-lg`}>
                 <Employer title="Accenture">
                     <Project 
-                        title="Bid Optimization" 
+                        title="Bid optimization" 
                         techStack={['React', 'Typescript', 'AWS Dynamo', 'AWS Cloudwatch', 'AWS Lamdba', 'AWS Amplify', 'AWS Scheduler', 'Python', 'Pandas', 'Vitest', 'Cypress', 'Accenture Manage MySales (MMS)', 'Salesforce']}
+                        years={{start: 2024, continuing: true}}
                         >
                         Some details about clio.
                     </Project>
+                    <Project 
+                        title="Branded component library" 
+                        techStack={['React', 'Typescript', 'React-Aria', 'a11y']}
+                        years={{start: 2024, continuing: true}}
+                        >
+                        Some details about muse.
+                    </Project>
+                    <Project 
+                        title="E2E testing warehouse utilization" 
+                        techStack={['React', 'Typescript', 'AWS S3', 'Terraform', 'Jest', 'Cypress']}
+                        years={{start: 2024, continuing: false}}
+                        >
+                        Some details about How I tested the rastorization process.
+                    </Project>
+                    <Project 
+                        title="Employee project utilization planner" 
+                        techStack={['Angular', 'Typescript', 'RxJs', 'Redux', 'Sub-Pub']}
+                        years={{start: 2022, end: 2023, continuing: false}}
+                        >
+                        Talk about online spreadsheets.
+                    </Project>
                 </Employer>
-                <hr className="sm:invisible" />
                 <div className="w-full">
                     Gensuite / Benchmark
                 </div>
-                <hr className="sm:invisible" />
                 <div className="w-full">
                     Cornerstone
                 </div>
--- src/app/resume/projects/projections.tsx
+++ src/app/resume/projects/projections.tsx
@@ -1,31 +1,55 @@
+export type Years = {
+    start: number;
+    end?: number;
+    continuing: boolean;
+}
+
 export const Project = ({
     title, 
     techStack,
-    children
+    children,
+    years
 }:{
     title: string, 
     techStack: string[],
-    children: React.ReactNode
+    children: React.ReactNode,
+    years: Years,
 }) => {
     return (
-        <div className="flex flex-col p-2 gap-2">
-            <h3 className="font-bold text-(--light-secondary) text-xl">
-                {title}
+        <div className="flex flex-col sm:p-2 max-sm:py-4 gap-2">
+            <h3 className="font-bold text-(--light-secondary) text-xl max-sm:underline">
+                {title} ({yearsTag(years)})
             </h3>
             <div className="flex ">
                 {children}
             </div>
+            <p className="text-(--light-tertiary)">
+                {techStack.join(', ')}
+            </p>
         </div>
     )
 }
 
+const yearsTag = (years: Years) => {
+
+    if (years.continuing){
+        return `${years.start} - Present`;
+    }
+    
+    if (!years.continuing && !years.end){
+        return `${years.start}`;
+    }
+
+    return `${years.start} - ${years.end}`;
+}
+
 export const Employer = ({title, children}:{title: string, children: React.ReactNode}) => {
     return (
-        <div className="flex flex-col gap-2 border-1 border-dashed rounded-sm border-(--dark-primary) border-rounded hover:border-(--light-primary)">
-            <h2 className="font-bold text-(--light-tertiary) text-2xl">
+        <div className="flex flex-col p-6 max-sm:p-8 gap-2 border-1 border-dashed rounded-sm border-(--dark-primary) border-rounded hover:border-(--light-primary)">
+            <h2 className="font-bold text-2xl underline">
                 {title}
             </h2>
-            <div className="flex ">
+            <div className="flex flex-col">
                 {children}
             </div>
         </div>
