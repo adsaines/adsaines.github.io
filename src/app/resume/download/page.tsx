b072753d28a6e4ebbfc4e61f111853f861c1e13b
Alex Saines <46796104+adsaines@users.noreply.github.com>
2026-04-27 23:56:23 -0400

    AI showed me a greate way to make a resume generator

---
+++ src/app/resume/download/page.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from "next"
+
+/*
+    TODO: introduce dynamic resume download
+    - a thorough one
+    - a simple one
+    - one with an accepted template header for submissions to jobs
+
+    Useing some URL variables I should be able to stuff some specifics in there.
+*/
+
+const Download: NextPage = () => {
+    return (
+        <div>
+            Check the Gemini chat "Dynami PDF Generation." It describes a way to use my existing page data to download different resume types from the browser.
+        </div>
+    )
+}
+
+export default Download;
