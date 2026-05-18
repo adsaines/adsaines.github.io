178df8098a891ee4a5bfcc497f1c963c6f7650d3
Alex Saines <46796104+adsaines@users.noreply.github.com>
2026-05-17 23:31:35 -0400

    updated onai page to be sub-sections instead of side-by side bullet points

--- src/app/onai/page.tsx
+++ src/app/onai/page.tsx
@@ -1,90 +1,87 @@
 import { NextPage } from "next"
-import { ItemWithExplanations, StandardResumeSection } from "../resume/projections"
+import { FormatTalkingPoint, ItemWithExplanations, StandardResumeSection } from "../resume/projections"
 
 const OnAi: NextPage = () => {
     return (    
-        <div className="h-full overflow-auto flex justify-center mb-4">
+        <div className="h-full overflow-auto px-4 flex justify-center mb-4">
             <article className={`resume flex flex-col md:p-4 gap-4 m-2 md:max-w-4/5 sm:w-full text-lg`}>
-                <StandardResumeSection title="On AI">
-                    <ItemWithExplanations 
-                        title="Facts"
-                        subTitle="...that I find important"
-                        talkingPoints={[
-                            {
+                <StandardResumeSection title="Facts">
+                    <div className="flex flex-col md:px-6 gap-6">
+                        <FormatTalkingPoint 
+                            point={{
                                 bolded: 'AI makes work go fast',
                                 followUp: 'There is no denying that AI increases the velocity of software engineering by finding fixes quicker, seeing what is wrong before testing, and generating code far faster than you can type.',
-                            },
-                            {
+                            }} 
+                            />
+                        <FormatTalkingPoint 
+                            point={{
                                 bolded: 'AI is not your friend',
                                 followUp: 'At the moment (mid 2026), AI models all tend towards sycophantic behavior and agree with you as much as they can. They want you to be happy because they are part of a business model. Their ultimate goal is to please you, not do things correctly.',
-                            },
-                            {
+                            }} 
+                            />
+                        <ItemWithExplanations 
+                            title="Sycophantism"
+                            subTitle="Mirror behavior"
+                            talkingPoints={[{
+                                bolded: 'Yes, we know it does this',
+                                followUp: 'Yes, this happens a lot, and everyone is susceptible.'
+                            }]}
+                            link={{
+                                title: 'AI overly affirms users asking for personal advice',
+                                href: 'https://news.stanford.edu/stories/2026/03/ai-advice-sycophantic-models-research'
+                            }}
+                            />
+                        <FormatTalkingPoint 
+                            point={{
                                 bolded: 'AI uses lots of energy',
                                 followUp: 'A simple prompt asking a basic question uses the same amount of energy as "powering a 9W LED light bulb for 2 to 20 minutes." That is the answer I got asking Gemini how much energy it uses for my queries. A basic google search agrees. The more complex the query the more power used and this is causing a serious strain on the power grid.',
-                            }
-                        ]}
-                        />
-                    <ItemWithExplanations 
-                        title="Warnings"
-                        subTitle="...about possible pitfalls"
-                        talkingPoints={[
-                            {
+                            }} 
+                            />
+                    </div>
+                </StandardResumeSection>
+
+                <StandardResumeSection title="Warnings">
+                    <div className="flex flex-col md:px-6 gap-6">
+                        <FormatTalkingPoint 
+                            point={ {
                                 bolded: 'AI can miss important points',
                                 followUp: `At the moment AI does not want to read your whole code base, and you shouldn't let it. It can take a huge amount of energy and time for it to load everything you have done. This means that when you are making modifications and additions to an existing code base you must know what you are doing and what already exists. That way you can advise the AI on the most precise methods to accomplish the task with the minimal amount of repetition.`,
-                            },
-                            {
+                            }} 
+                            />
+                        <FormatTalkingPoint 
+                            point={{
                                 bolded: 'AI knows what you do not, and does not know what you do',
                                 followUp: `AI knows lots of things related to software engineering and the common practices of existing languages. AI does not know how you want to do your project. If you ask any model for suggestions on a specific topic, it will tell you what it has been trained on: other peoples knowledge. For common applications that fine, but any business specific desires, artistic decisions, or personal preferences are not included in it's opinon.`,
-                            },
-                            {
+                            }} 
+                            />
+                        <FormatTalkingPoint 
+                            point={{
                                 bolded: 'AI learns from you',
-                                followUp: `If you are using a public model, that model is taking your inputs and re-training itself on them. That means that any information that you are asking it to look at becomes a form of public knowledge. That includes your methodologies and actual words / code. All of the text on my resume is part of Gemini now.`,
-                            },
-                        ]}
-                        />
-                    <ItemWithExplanations 
-                        title="Our ethical responsibility"
-                        subTitle="...to the people affected"
-                        talkingPoints={[
-                            {
+                                followUp: `If you are using a public model, that model is taking your inputs and re-training itself on them. That means that any information that you are asking it to look at becomes a form of public knowledge. That includes your methodologies and actual words / code. Example: all of the text on my resume is part of Gemini now.`,
+                            }} 
+                            />
+                    </div>
+                </StandardResumeSection>
+
+                <StandardResumeSection title="Our ethical responsibility">
+                    <div className="flex flex-col md:px-6 gap-6">
+                        <FormatTalkingPoint 
+                            point={{
                                 bolded: `AI is a tool`,
                                 followUp: `AI should not replace people. Humans should remain the ones making decisions and AI can be used to implement them. We should use the impressive savings and gains of AI to fuel a more leisurely life for all the people of the world.`,
-                            },
-                            {
+                            }} 
+                            />
+                        <FormatTalkingPoint 
+                            point={{
                                 bolded: `AI should not be a weapon`,
-                                followUp: `I know that this is me yelling into the void, but AI should not be used to power weapons. As soon as a non-human model makes a decision in the life or death of a Human we have willfully chosen to forfeit our morality. War exists, and the ultimate power is the power to kill, because those who are unwilling to kill will always be beholden to those who are. If we give the power to kill to a system that does not have humans as the final deciders; then we as a species have given the lives of all those unwilling to kill, to the system that is.`,
-                            },
-                        ]}
-                        />
+                                followUp: `I know that this is me yelling into the void, but AI should not be used to power weapons. As soon as a non-human makes a decision in the life or death of a human we have willfully chosen to forfeit our morality. War exists, and the ultimate power is the power to kill, because those who are unwilling to kill will always be beholden to those who are. If we give the power to kill to a system that does not have humans as the final deciders; then we as a species have given the lives of all those unwilling to kill, to a system that is.`,
+                            }} 
+                            />
+                    </div>
                 </StandardResumeSection>
                 
                 <StandardResumeSection title="AI on itself">
-                    <div className="flex flex-col max-sm:gap-4 md:gap-6 md:p-6 justify-start">
-                        <p>
-                            After writing the "on AI" section above I started thinking philosophically and ended up asking AI...
-                        </p>
-                        <div className="md:px-10 text-(--light-tertiary)">
-                            <p>
-                                "
-                            </p>
-                            <p>
-                                Given that the usage of AI, of you, is unanimous with software engineering at the moment I thought that it would be good to add an "on AI" section to my resume page. It has... deviated from the original plan.
-                            </p>
-                            <p>
-                                As an AI, how would you go about telling others of your strengths, flaws, limitations, and purpose?
-                            </p>
-                            <p>
-                                "
-                            </p>
-                        </div>
-                        <p>
-                            It's response is below. I saw that it had included some of the same topics that I was hitting. It was validating to know that I had not hallucinated those items. That being said, it did remind me of it's core <a className="text-(--internal-link) hover:underline max-sm:underline" href="https://news.stanford.edu/stories/2026/03/ai-advice-sycophantic-models-research" target="_blank">sycophantic</a> behavior by sharing the same ideas that I had. All in a all a rather circular thought train.
-                        </p>
-                    </div>
-                    <div className="flex flex-col max-sm:gap-4 md:gap-6 md:p-6 justify-start md:px-16">
-                        <p>
-                            "
-                        </p>
+                    <div className="flex flex-col max-sm:gap-4 md:gap-6 justify-start md:px-6">
                         <h2 className="text-(--light-secondary) font-bold">
                             An AI's User Manual: Purpose, Strengths, Flaws, and Boundaries
                         </h2>
@@ -152,9 +149,6 @@ const OnAi: NextPage = () => {
                         </p>
                         <p>
                             In this developer's workflow, I am the engine room—but they are the navigator, the architect, and the final judge of quality.
-                        </p>
-                        <p>
-                            "
                         </p>
                     </div>
                 </StandardResumeSection>
